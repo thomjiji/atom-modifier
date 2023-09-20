254863988d3f9f9e91c69750d4acc28e164a144c
thomjiji <hxy449999@gmail.com>
2023-09-20 13:04:01 +0800

    The position for colr is seeked and not hardcoded.
     But after the modification, the movie file is not valid anymore. ^^!

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,50 @@
 use std::fs::OpenOptions;
-use std::io;
 use std::io::{Read, Seek, SeekFrom, Write};
 
-fn main() -> io::Result<()> {
-    find_hdlr()
+fn main() -> std::io::Result<()> {
+    find_and_write_atom(
+        "/Users/thom/Desktop/video_stream_oneframe_modified.mov",
+        "colr",
+    )
+    .map_err(|e| {
+        eprintln!("Failed to find and write nclc: {}", e);
+        e
+    })
 }
 
-fn find_hdlr() -> io::Result<()> {
-    // Open file with write permissions
-    let mut f = OpenOptions::new()
+fn find_and_write_atom(input_file_path: &str, atom: &str) -> std::io::Result<()> {
+    let mut file = OpenOptions::new()
         .read(true)
         .write(true)
-        .open("/Users/thom/Desktop/video_stream_oneframe_modified.mov")?;
+        .open(input_file_path)?;
 
-    // Seek to the position
-    let position_to_modify = 0x8fe30 + 11;
-    // The position is adjusted counting bytes beginning from offset 0x8fe30.
-    f.seek(SeekFrom::Start(position_to_modify as u64))?;
-
-    // Write new data
-    f.write_all(&[0x01, 0x00, 0x01, 0x00, 0x01])?;
+    if let Some(position) = find_string_position(&mut file, atom.as_bytes())? {
+        write_bytes_at(&mut file, position + 11, &[0x01, 0x00, 0x01, 0x00, 0x01])?;
+    } else {
+        eprintln!("Did not find the string to modify.");
+    }
 
     Ok(())
+}
+
+fn find_string_position(f: &mut std::fs::File, s: &[u8]) -> std::io::Result<Option<u64>> {
+    let mut buffer = [0; 1]; // Buffer to hold one byte
+    let mut offset = 0;
+
+    while f.read(&mut buffer)? > 0 {
+        if buffer[0] == s[offset] {
+            offset += 1;
+            if offset == s.len() {
+                return Ok(Some(f.stream_position()? - s.len() as u64)); // Here the pattern ends
+            }
+        } else {
+            offset = 0;
+        }
+    }
+    Ok(None) // Could not find the string
+}
+
+fn write_bytes_at(f: &mut std::fs::File, position: u64, bytes: &[u8]) -> std::io::Result<()> {
+    f.seek(SeekFrom::Start(position))?;
+    f.write_all(bytes)
 }
