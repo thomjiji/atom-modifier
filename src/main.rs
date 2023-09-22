12da84090833e1c4de2ff045f970305a4365db68
thomjiji <hxy449999@gmail.com>
2023-09-22 18:40:03 +0800

    Handled command line arguments.
    Take three inputs: file path, colr tag (1-2-1), gama (2.4, 2,2 or 0). The expected values are not implemented.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,64 @@
-use std::fs::OpenOptions;
+use std::fs::{File, OpenOptions};
 use std::io::{Read, Seek, SeekFrom, Write};
+use std::{env, process};
 
-fn main() -> std::io::Result<()> {
-    find_and_write_atom(
-        "/Users/thom/Desktop/video_stream_oneframe_modified.mov",
-        "gama",
-    )
-    .map_err(|e| {
-        eprintln!("Failed to find and write nclc: {}", e);
-        e
-    })
+struct Video {
+    file: File,
+    size: u64,
+    offset: u64,
+    cursor: u64,
 }
 
+// impl Video {
+//     fn new(input_file: &File) {
+//         Video {
+//             file: input_file,
+//             size: input_file.metadata()?.len(),
+//             offset: 0,
+//             cursor: 0,
+//         };
+//     }
+// }
+
+#[derive(Debug)]
+struct Config {
+    file_path: File,
+    colr: String,
+    gama: String,
+}
+
+impl Config {
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
+        let file_path = File::open(file_path).expect("Failed to open file");
+
+        let colr = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a colr tag"),
+        };
+
+        let gama = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get gama tag"),
+        };
+
+        Ok(Config {
+            file_path,
+            colr,
+            gama,
+        })
+    }
+}
+
+/// write_bytes_at(&mut file, position + 6, &[0x33, 0x33, 0x00, 0x00])?;  // change gama to 2.2
+/// write_bytes_at(&mut file, position + 6, &[0x66, 0x66, 0x00, 0x00])?;  // change gama to 2.4
+/// write_bytes_at(&mut file, position, &[0x00, 0x00, 0x00, 0x00])?;      // remove gama atom
 fn find_and_write_atom(input_file_path: &str, atom: &str) -> std::io::Result<()> {
     let mut file = OpenOptions::new()
         .read(true)
@@ -19,13 +66,11 @@ fn find_and_write_atom(input_file_path: &str, atom: &str) -> std::io::Result<()>
         .open(input_file_path)?;
 
     if let Some(position) = find_atom_position(&mut file, atom.as_bytes())? {
-        // write_bytes_at(&mut file, position + 11, &[0x01, 0x00, 0x01, 0x00, 0x01])?;
-        write_bytes_at(&mut file, position + 6, &[0x33, 0x33, 0x00, 0x00])?;
+        write_bytes_at(&mut file, position + 12, &[0x01, 0x00, 0x01, 0x00, 0x01])?; // change from 1-2-1 to 1-1-1
+        println!("{position}");
     } else {
         eprintln!("Did not find the string to modify.");
     }
-
-    println!("{:?}", atom.as_bytes());
 
     Ok(())
 }
@@ -66,4 +111,22 @@ fn find_atom_position(f: &mut std::fs::File, s: &[u8]) -> std::io::Result<Option
 fn write_bytes_at(f: &mut std::fs::File, position: u64, bytes: &[u8]) -> std::io::Result<()> {
     f.seek(SeekFrom::Start(position))?;
     f.write_all(bytes)
+}
+
+fn main() {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("{:#?}", config);
+
+    // find_and_write_atom(
+    //     "/Users/thom/code/rust/atom_modifier/test_footages/1-2-1_modified.mov",
+    //     "colr",
+    // )
+    // .map_err(|e| {
+    //     eprintln!("Failed to find and write nclc: {}", e);
+    //     e
+    // })
 }
