05fb12c1167cc9ab2dc8eed2a446508c1f0fb539
thomjiji <hxy449999@gmail.com>
2023-09-23 21:21:36 +0800

    Starting improve error handling. Not complete yet.
    Try to simplify the result type of ColrAtom.search().

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+use std::fmt::Formatter;
 use std::fs::File;
-use std::io::{BufReader, Read, Seek, SeekFrom, Write};
+use std::io::{BufReader, Error, Read, Seek, SeekFrom, Write};
 
 use clap::Parser;
 
@@ -14,7 +15,7 @@ struct Args {
     gama: String,
 }
 
-static COLR_ATOM: [u8; 4] = [0x63, 0x6F, 0x6C, 0x72]; // "colr"
+static COLR_ATOM: [u8; 4] = [0x63, 0x6f, 0x6c, 0x72]; // "colr"
 static GAMA_ATOM: [u8; 4] = [0x67, 0x61, 0x6d, 0x61]; // "gama"
 
 #[derive(Debug)]
@@ -25,9 +26,32 @@ enum ColorParameterType {
 }
 
 #[derive(Debug)]
+pub enum SearchError {
+    Io(Error),
+    NotFound(Vec<u8>),
+}
+
+impl From<Error> for SearchError {
+    fn from(err: Error) -> Self {
+        SearchError::Io(err)
+    }
+}
+
+impl std::fmt::Display for SearchError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            SearchError::Io(err) => write!(f, "IO error: {}", err),
+            SearchError::NotFound(pattern) => {
+                write!(f, "Pattern {:?} was not found in the file.", pattern)
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
 struct ColrAtom {
     size: u32,
-    parameter_type: ColorParameterType,
+    color_parameter_type: ColorParameterType,
     offset: u64,
     primaries: u16,
     transfer_function: u16,
@@ -38,14 +62,14 @@ impl ColrAtom {
     fn new() -> ColrAtom {
         Self {
             size: 0,
-            parameter_type: ColorParameterType::Nclc,
+            color_parameter_type: ColorParameterType::Nclc,
             offset: 0,
             primaries: 0,
             transfer_function: 0,
             matrix: 0,
         }
     }
-    fn search(file: &mut File, pattern: &[u8]) -> std::io::Result<Option<Self>> {
+    fn search(file: &mut File, pattern: &[u8]) -> Result<Option<Self>, SearchError> {
         let mut reader = BufReader::new(file);
         let mut buffer = vec![0; pattern.len()];
         let mut offset = 0;
@@ -53,21 +77,23 @@ impl ColrAtom {
         while reader.read(&mut buffer)? != 0 {
             if buffer.as_slice() == pattern {
                 let mut atom = Self::new();
-                atom.offset += offset;
-                reader.seek(SeekFrom::Current(-8))?;
+
+                atom.offset += offset; // set offset
+
+                reader.seek(SeekFrom::Current(-8))?; // The size of colr atom is 8 bytes before the colr's type.
                 let mut size_buf = [0; 4];
                 reader.read_exact(&mut size_buf)?;
-                atom.size += u32::from_be_bytes(size_buf);
+                atom.size += u32::from_be_bytes(size_buf); // set size
 
+                reader.seek(SeekFrom::Start(offset))?;
                 return Ok(Some(atom));
             }
             offset += 1;
             reader.seek(SeekFrom::Start(offset))?;
         }
 
-        let hex_pattern: Vec<String> = pattern.iter().map(|b| format!("{:02x}", b)).collect();
-        println!("Pattern {:?} was not found in the file.", hex_pattern);
-        Ok(None)
+        // println!("Pattern {:?} was not found in the file.", pattern);
+        Err(SearchError::NotFound(pattern.to_vec()))
     }
 }
 
@@ -85,6 +111,16 @@ fn main() {
     let mut stream =
         File::open("/Users/thom/code/rust/atom_modifier/test_footages/1-2-1_modified.mov")
             .expect("Failed to open the file");
-    let atom = ColrAtom::search(&mut stream, &COLR_ATOM);
-    println!("{:?}", atom);
+
+    match ColrAtom::search(&mut stream, &COLR_ATOM) {
+        Ok(Some(atom)) => {
+            println!("Found atom: {:?}", atom);
+        }
+        Ok(None) => {
+            println!("No atom found!");
+        }
+        Err(e) => {
+            println!("An error occurred: {}", e);
+        }
+    };
 }
