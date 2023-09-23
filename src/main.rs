5d91719c036604fa8545c04f1100e61b8fa15c25
thomjiji <hxy449999@gmail.com>
2023-09-23 13:01:26 +0800

    Search for colr atom using pattern match works!!!
    1. Still a lot of work to do. Now it can search for a specific atom pattern (like colr - 0x63, 0x6F, 0x6C, 0x72), return the offset of it. Then we can base on this offset to do more operations like overwriting the bytes follows it.
    
    2. Also implement args parsing functionality using clap which makes my life easier.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
-use std::fs::{File, OpenOptions};
-use std::io::{Read, Seek, SeekFrom, Write};
-use std::{env, process};
+use std::fs::File;
+use std::io::{BufReader, Read, Seek, SeekFrom, Write};
+
+use clap::Parser;
+
+use crate::ColorParameterType::Nclc;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    input_file_path: String,
+
+    #[arg(short, long)]
+    colr: String,
+
+    #[arg(short, long, default_value_t = String::from("0"))]
+    gama: String,
+}
+
+static COLR_ATOM: [u8; 4] = [0x63, 0x6F, 0x6C, 0x72]; // "colr"
+static NCLC: [u8; 4] = [0x6e, 0x63, 0x6c, 0x63]; // "colr"
 
 struct Video {
     file: File,
@@ -9,104 +28,58 @@ struct Video {
     cursor: u64,
 }
 
-// impl Video {
-//     fn new(input_file: &File) {
-//         Video {
-//             file: input_file,
-//             size: input_file.metadata()?.len(),
-//             offset: 0,
-//             cursor: 0,
-//         };
-//     }
-// }
-
-#[derive(Debug)]
-struct Config {
-    file_path: File,
-    colr: String,
-    gama: String,
+enum ColorParameterType {
+    Nclc, // for video
+    Prof, // for print
+    Unknown,
 }
 
-impl Config {
-    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
-        args.next();
+struct ColrAtom {
+    size: u8,
+    parameter_type: ColorParameterType,
+    offset: u64,
+    primaries: u16,
+    transfer_function: u16,
+    matrix: u16,
+}
 
-        let file_path = match args.next() {
-            Some(arg) => arg,
-            None => return Err("Didn't get a file path"),
-        };
-
-        let file_path = File::open(file_path).expect("Failed to open file");
-
-        let colr = match args.next() {
-            Some(arg) => arg,
-            None => return Err("Didn't get a colr tag"),
-        };
-
-        let gama = match args.next() {
-            Some(arg) => arg,
-            None => return Err("Didn't get gama tag"),
-        };
-
-        Ok(Config {
-            file_path,
-            colr,
-            gama,
-        })
+impl ColrAtom {
+    fn new() -> ColrAtom {
+        Self {
+            size: 0,
+            parameter_type: Nclc,
+            offset: 0,
+            primaries: 0,
+            transfer_function: 0,
+            matrix: 0,
+        }
     }
+    fn search(file: &mut File, pattern: &[u8]) -> std::io::Result<Option<u64>> {
+        let mut reader = BufReader::new(file);
+        let mut buffer = vec![0; pattern.len()];
+        let mut offset = 0;
+
+        while reader.read(&mut buffer)? != 0 {
+            if buffer.as_slice() == pattern {
+                return Ok(Some(offset));
+            }
+            offset += 1;
+            reader.seek(SeekFrom::Start(offset))?;
+        }
+
+        println!("Pattern was not found in the file.");
+        Ok(None)
+    }
+}
+
+struct GamaAtom {
+    size: u8,
+    data: u32,
 }
 
 /// write_bytes_at(&mut file, position + 6, &[0x33, 0x33, 0x00, 0x00])?;  // change gama to 2.2
 /// write_bytes_at(&mut file, position + 6, &[0x66, 0x66, 0x00, 0x00])?;  // change gama to 2.4
 /// write_bytes_at(&mut file, position, &[0x00, 0x00, 0x00, 0x00])?;      // remove gama atom
-fn find_and_write_atom(input_file_path: &str, atom: &str) -> std::io::Result<()> {
-    let mut file = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .open(input_file_path)?;
-
-    if let Some(position) = find_atom_position(&mut file, atom.as_bytes())? {
-        write_bytes_at(&mut file, position + 12, &[0x01, 0x00, 0x01, 0x00, 0x01])?; // change from 1-2-1 to 1-1-1
-        println!("{position}");
-    } else {
-        eprintln!("Did not find the string to modify.");
-    }
-
-    Ok(())
-}
-
-/// Find provided atom name's position, returned by decimal.
-fn find_atom_position(f: &mut std::fs::File, s: &[u8]) -> std::io::Result<Option<u64>> {
-    const BUFFER_SIZE: usize = 1024;
-    let mut buffer = [0; BUFFER_SIZE];
-
-    // The position in `s` that we're looking for in the buffer.
-    let mut offset = 0;
-
-    loop {
-        match f.read(&mut buffer)? {
-            0 => return Ok(None), // End of file, string not found
-            len => {
-                for (i, &byte) in buffer[..len].iter().enumerate() {
-                    if byte == s[offset] {
-                        offset += 1;
-                        if offset == s.len() {
-                            // Found the string, calculate position
-                            let buffer_pos = f.stream_position()? as usize;
-                            let pos = buffer_pos - BUFFER_SIZE + i + 1 - offset;
-                            return Ok(Some(pos as u64));
-                        }
-                    } else if offset > 0 {
-                        // If there was a partial match, we need to get back those bytes
-                        // and should step file cursor back.
-                        f.seek(SeekFrom::Current(-(offset as i64 - 1)))?;
-                        offset = 0;
-                    }
-                }
-            }
-        }
-    }
-}
 
 fn write_bytes_at(f: &mut std::fs::File, position: u64, bytes: &[u8]) -> std::io::Result<()> {
     f.seek(SeekFrom::Start(position))?;
@@ -114,19 +87,17 @@ fn write_bytes_at(f: &mut std::fs::File, position: u64, bytes: &[u8]) -> std::io
 }
 
 fn main() {
-    let config = Config::build(env::args()).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {err}");
-        process::exit(1);
-    });
+    let mut stream =
+        File::open("/Users/thom/code/rust/atom_modifier/test_footages/1-2-1_modified.mov")
+            .expect("Failed to open the file");
+    let x = ColrAtom::search(&mut stream, &COLR_ATOM);
+    println!("{:?}", x);
 
-    println!("{:#?}", config);
-
-    // find_and_write_atom(
-    //     "/Users/thom/code/rust/atom_modifier/test_footages/1-2-1_modified.mov",
-    //     "colr",
-    // )
-    // .map_err(|e| {
-    //     eprintln!("Failed to find and write nclc: {}", e);
-    //     e
-    // })
+    // let mut stream = config.read_file("/Users/thom/Desktop/1-1-1.mov").unwrap();
+    // let mut buffer = Vec::new();
+    // let x = stream.read_to_end(&mut buffer).unwrap();
+    //
+    // for byte in buffer.iter().take(100) {
+    //     println!("{} ", byte);
+    // }
 }
